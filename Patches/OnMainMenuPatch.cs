a81295a8b3a0d46c8a2303eb72d5b5b2c616b84a
NotDifficult <89353219+notdifficult@users.noreply.github.com>
2026-07-25 02:54:46 +0700

    2.0.0

--- Patches/OnMainMenuPatch.cs
+++ Patches/OnMainMenuPatch.cs
@@ -1,1 +1,19 @@
-
+﻿//======================================================================================================================//
+using System.Reflection;
+using SPT.Reflection.Patching;
+using HarmonyLib;
+//======================================================================================================================//
+namespace MiyukiMainMenuMusic.Patches { internal class OnMainMenuPatch : ModulePatch { protected override MethodBase GetTargetMethod() { return AccessTools.Method(typeof(MainMenuControllerClass), nameof(MainMenuControllerClass.method_5)); }
+//======================================================================================================================//
+        [PatchPostfix] private static void Postfix(MainMenuControllerClass __instance)
+        {
+            //Logger.LogWarning("[MiyukiMainMenuMusic] OnMainMenuPatch.cs");                                            //Plugin.Log.LogInfo("[MiyukiMainMenuMusic] MainMenu");  //Plugin.Log.LogInfo("MMCC.method_5 ran");
+            if (Plugin.Instance == null) { Logger.LogError("[MiyukiMainMenuMusic] ModMain.Instance ещё не готов!"); return; }
+            var player = Plugin.Instance.GetOrCreatePlayer();                                                           // 1. Получаем или создаём плеер (если его ещё нет)
+            if (player == null) return;
+            player.SetTracks(Plugin.Instance.GetPreparedTracks());
+            Plugin.Instance.StartMusic();                                                                               // 3. ЗАПУСКАЕМ МУЗЫКУ Внутри StartMusic произойдёт: передача треков -> загрузка файлов (LoadTracksAsync) -> MusicPlay()
+        }
+    }
+}
+//======================================================================================================================//
---
+++ Patches/OnRaidStartPatch.cs
@@ -0,0 +1,28 @@
+﻿//======================================================================================================================//
+using System.Reflection;
+using SPT.Reflection.Patching;
+using EFT;
+using HarmonyLib;
+using UnityEngine;
+using MiyukiMainMenuMusic; 
+//======================================================================================================================//
+namespace MiyukiMainMenuMusic.Patches { internal class OnRaidStartPatch : ModulePatch {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(GameWorld), nameof(GameWorld.OnGameStarted));
+            //return typeof(GameWorld).GetMethod(nameof(GameWorld.OnGameStarted));
+        }
+//======================================================================================================================//
+        /*[PatchPostfix] public static void PatchPostfix()
+        {
+            Plugin.Log.LogInfo("[MiyukiMainMenuMusic] Raid starting");
+            Logger.LogWarning("[MiyukiMainMenuMusic] OnRaidStartPatch.cs");
+        }*/
+        [PatchPostfix] public static void PatchPostfix()
+        //[PatchPrefix] private static bool Prefix()
+        {
+            Logger.LogWarning("[MiyukiMainMenuMusic] OnRaidStartPatch.cs");
+            Plugin.Instance.StopMusic();
+        }
+    }
+}
