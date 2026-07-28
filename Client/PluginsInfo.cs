12652b3f581d49ffeca6268c097ca6a176bd01a4
NotDifficult <89353219+notdifficult@users.noreply.github.com>
2026-07-28 18:14:54 +0700

    2.0.0

---
+++ Client/PluginsInfo.cs
@@ -0,0 +1,11 @@
+﻿//====================================================================================================================//
+namespace MiyukiMainMenuMusic { public static class PluginsInfo
+    {
+        public const string GUID = "NoTDifficult.MiyukiMainMenuMusic";
+        public const string NAME = nameof(MiyukiMainMenuMusic);
+        public const string VERSION = "2.0.0";
+        private const string LogPrefix   = "[MiyukiMainMenuMusic]";
+        public static string Format(string message) { return $"[{LogPrefix}] {message}"; }
+    }
+}
+//====================================================================================================================//
